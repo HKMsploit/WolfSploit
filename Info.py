cced78654edb57d0e76ade932f4c617783b78e61
HKMsploit <98546707+HKMsploit@users.noreply.github.com>
2022-01-27 20:04:02 +0200

    Add files via upload

---
+++ Info.py
@@ -0,0 +1,32 @@
+#---------------« colours »---------------
+
+R = '\033[1;31m' #احمر
+Y = '\033[1;33m' #اصفر
+R2 = '\033[2;31m' #احمر ثاني
+G = '\033[2;32m' #اخضر
+B = '\033[2;34m'#ازرق
+P = '\033[2;35m' #وردي
+C = '\033[2;36m'#سمائي
+BW = '\033[1;34m' #ازرق فاتح
+
+#-------------« the message »-------------
+
+print (C+'-----------------------«( WolfSploit )»--------------------')
+
+print (P+'>>>>>>> How to use WolfSploit ? <<<<<<<<')
+print (P+'.')
+print (P+'.')
+print (P+'.')
+print (P+'↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓')
+
+print (C+'1 ====> Create a New bot in Telegram')
+
+print (Y+'2 ====> Open the script')
+
+print (G+'3 ====> Put your Telegram ID ')
+
+print (R+'4 ====> Put your Telegram bot token') 
+
+print (C+'5 ====> Put you listword script')
+
+print (B+'6 ====> Receiving Instgram Accounts in Telegram bot')
